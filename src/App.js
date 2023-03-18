6051f5aa4960135fe10993200a93e38cfe6f4b8a
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2023-03-18 23:47:51 +0800

    push to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput]=useState()
+  const [print, setPrint]=useState(false)
+  const [msg, setMsg]=useState()
+  const msgHandler = (e) =>{
+    setInput(e.target.value)
+  }
+  const printMsg = ()=> {
+    setPrint(true)
+    setMsg(input)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    <div className='flex fixed justify-center w-[100%] h-[60px] bg-[#242526] shadow-md'>
+      <p className='font-[serif] font-[500] text-[40px] text-[#ffffff]'>Display message</p>
     </div>
+
+    <div className='flex flex-row justify-center items-center m-[30] bg-[#18191A] w-screen h-screen'>
+
+      <div className='flex flex-col p-8 gap-5 bg-[#242526] w-[300px] h-[400px] shadow-xl rounded-md '>
+          <input 
+            className='rounded-sm h-[35px] pl-[10px] outline-none' type='text' required placeholder='your message'
+            onChange={msgHandler}
+          />
+        <button 
+          className='bg-[#EB6100] rounded-sm h-[35px] text-[#ffffff] hover:opacity-80 active:scale-90'
+          onClick={printMsg}
+        >Print</button>
+      {print &&
+        <div>
+          <p>{msg}</p>
+        </div>
+      }
+      </div>
+
+    </div>
+
+    </>
   );
 }
 
